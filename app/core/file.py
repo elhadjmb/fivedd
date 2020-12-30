6fa3cea9968513f41313c737cbc25296365b8f7c
Hadj <hadjmahmoudbederina@gmail.com>
2020-12-30 22:32:43 +0100

    added initial File attributes

---
+++ app/core/file.py
@@ -0,0 +1,10 @@
+import os.path
+
+
+class File:
+    def __init__(self, path="/"):
+        self.path = r"{}".format(path)  # if Checker.path(path) else Exceptions.flag(type=Dictionary.internal())
+        self.file_name = os.path.basename(self.path)
+        self.size = os.path.getsize(self.path)
+        self.extension = os.path.splitext(self.path)[1]
+        self.name = self.file_name.replace(self.extension, "")
