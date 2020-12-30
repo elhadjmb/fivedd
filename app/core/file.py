880f47230d9e6c17f1cd62a3d3f3ec326a236ada
Hadj <hadjmahmoudbederina@gmail.com>
2020-12-30 22:52:36 +0100

    added checker.py dictionary.py exceptions.py

--- app/core/file.py
+++ app/core/file.py
@@ -3,7 +3,7 @@ import os.path
 
 class File:
     def __init__(self, path="/"):
-        self.path = r"{}".format(path)  # if Checker.path(path) else Exceptions.flag(type=Dictionary.internal())
+        self.path = r"{}".format(path)  # if Checker.path(path) else Exceptions.flag(type=Dictionary.Internal())
         self.file_name = os.path.basename(self.path)
         self.size = os.path.getsize(self.path)
         self.extension = os.path.splitext(self.path)[1]
